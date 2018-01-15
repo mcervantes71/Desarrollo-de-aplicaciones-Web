e37390e57d5fc613fdb72f02838cf1cba535f4a5
Martin Cervantes <cervantes.martine@gmail.com>
2018-01-14 19:53:36 -0700

    imc

---
+++ C#/IMC/IMC/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace IMC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(151, 46);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 26);
+            this.textBox1.TabIndex = 0;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(151, 117);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 26);
+            this.textBox2.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Peso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(59, 120);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(70, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Estatura";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(74, 186);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Calcular";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Calcular);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(329, 278);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "IMC";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ C#/IMC/IMC/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IMC
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Calcular(object sender, EventArgs e)
+        {
+            double peso = Convert.ToSingle(textBox1.Text);
+            double estatura = Convert.ToSingle(textBox2.Text);
+
+            double imc = peso / (estatura * estatura);
+
+            if(imc < 20)
+            {
+                MessageBox.Show("Su IMC = " + imc + "\nBajo Peso", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (imc < 25)
+            {
+                MessageBox.Show("Su IMC = " + imc + "\nPeso Normal", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else if (imc < 30)
+            {
+                MessageBox.Show("Su IMC = " + imc + "\nSobre Peso", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            }
+            else if (imc < 35)
+            {
+                MessageBox.Show("Su IMC = " + imc + "\n1o de Obesidad", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else if (imc < 40)
+            {
+                MessageBox.Show("Su IMC = " + imc + "\n2o de Obesidad", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+            else if(imc <= 40)
+            {
+                MessageBox.Show("Su IMC = " + imc + "\n3o de Obesidad", "IMC", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+            }
+        }
+    }
+}
