cdd5d7d0faa291a874cbf2549e94679dfcf09484
Martin Cervantes <cervantes.martine@gmail.com>
2018-02-12 13:53:13 -0700

    fixed

--- jQuery/PokeAPI/js/script.js
+++ jQuery/PokeAPI/js/script.js
@@ -1,9 +1,9 @@
 $(function(){
-    var pokeURL = "http://pokeapi.co/api/v2/generation/1";
-    var pokemonByName = "http://pokeapi.co/api/v2/pokemon/"
+    var pokeURL = "https://pokeapi.co/api/v2/generation/1";
+    var pokemonByName = "https://pokeapi.co/api/v2/pokemon/"
 
     $.getJSON(pokeURL).done(function(data){
-        // console.log(data);
+        console.log(data);
 
         $.each(data.pokemon_species, function(index, pokemon){
             var name = pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1);
@@ -12,13 +12,13 @@ $(function(){
 
             link.click(function(event){
                 $.getJSON(pokemonByName + pokemon.name).done(function(details){
-                    //console.log(details);
+                    console.log(details);
                     $("#pokemon-details").empty();
                     $("#pokemon-details").append("<h2>" + name + "</h2>");
-                    $("#pokemon-details").append("<img src'" + details.sprites.front_default + "'>");
-                    $("#pokemon-details").append("<img src'" + details.sprites.back_default + "'>");
-                    $("#pokemon-details").append("<img src'" + details.sprites.front_shiny + "'>");
-                    $("#pokemon-details").append("<img src'" + details.sprites.back_shiny + "'>");
+                    $("#pokemon-details").append("<img src='" + details.sprites.front_default + "'>");
+                    $("#pokemon-details").append("<img src='" + details.sprites.back_default + "'>");
+                    $("#pokemon-details").append("<img src='" + details.sprites.front_shiny + "'>");
+                    $("#pokemon-details").append("<img src='" + details.sprites.back_shiny + "'>");
 
                     event.preventDefault();
                 });
