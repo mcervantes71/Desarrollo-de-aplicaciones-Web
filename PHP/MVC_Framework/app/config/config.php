f3350a42c181daeebd7312f9e3916dbd91ee5f6a
Martin Cervantes <cervantes.martine@gmail.com>
2018-02-21 13:40:27 -0700

    MVC Framework ;)

---
+++ PHP/MVC_Framework/app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+    // App Root
+    define('APP_ROOT', dirname(dirname(__FILE__)));
+    // URL Root
+    define('URL_ROOT', '_YOUR_URL_');
+    // Site Name
+    define('SITE_NAME', '_YOUR_SITENAME_');
+
+    // DB Params
+    define('DB_HOST', 'localhost');
+    define('DB_USER', '_YOUR_USER_');
+    define('DB_PASS', '_YOUR_PASS_');
+    define('DB_NAME', '_YOUR_DBNAME_');
