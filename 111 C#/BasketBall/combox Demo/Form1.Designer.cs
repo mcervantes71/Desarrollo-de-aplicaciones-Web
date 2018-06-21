0d2d94eafacb9044e8d932352ca705e3e5cef55b
Martin Cervantes <cervantes.martine@gmail.com>
2018-06-21 12:24:28 -0600

    score keeper

---
+++ 111 C#/BasketBall/combox Demo/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace combox_Demo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Este",
+            "Oeste"});
+            this.comboBox1.Location = new System.Drawing.Point(46, 58);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(180, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(262, 58);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(180, 28);
+            this.comboBox2.TabIndex = 1;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(478, 58);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(289, 28);
+            this.comboBox3.TabIndex = 2;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(46, 20);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Conferencia";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(266, 14);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(63, 20);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "División";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(474, 14);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(67, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Equipos";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::combox_Demo.Properties.Resources.Mavericks;
+            this.pictureBox1.Location = new System.Drawing.Point(83, 163);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(307, 164);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(816, 474);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.comboBox1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "BasketBall";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ 111 C#/BasketBall/combox Demo/Form1.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace combox_Demo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void Clear(Boolean Combo2Changed)
+        {
+            if (Combo2Changed)
+            {
+                comboBox2.Text = "";
+                comboBox2.Items.Clear();
+            }
+
+            comboBox3.Text = "";
+            comboBox3.Items.Clear();
+
+            pictureBox1.Visible = false;
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Clear(true);
+
+            if (comboBox1.SelectedIndex == 0)
+            {
+                comboBox2.Items.Add("Atlántico");
+                comboBox2.Items.Add("Central");
+                comboBox2.Items.Add("Sureste");
+            }
+            else
+            {
+                comboBox2.Items.Add("Noroeste");
+                comboBox2.Items.Add("Pacífico");
+                comboBox2.Items.Add("Suroeste");
+            }
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Clear(false);
+
+            if (comboBox2.SelectedItem.ToString() == "Noroeste")
+            {
+                comboBox3.Items.Add("Denver - Nuggets");
+                comboBox3.Items.Add("Minnesota - Timberwolves");
+                comboBox3.Items.Add("Seattle - SuperSonics");
+                comboBox3.Items.Add("Portland - Trail Blazers");
+                comboBox3.Items.Add("Utah - Jazz");
+            }
+            else if(comboBox2.SelectedItem.ToString() == "Pacífico")
+            {
+                comboBox3.Items.Add("San Francisco - Warriors");
+                comboBox3.Items.Add("San Diego - Clippers");
+                comboBox3.Items.Add("Los Angeles - Lakers");
+                comboBox3.Items.Add("Phoenix - Suns");
+                comboBox3.Items.Add("Sacramento - Kings");
+            }
+            else if(comboBox2.SelectedItem.ToString() == "Suroeste")
+            {
+                comboBox3.Items.Add("Dallas - Mavericks");
+                comboBox3.Items.Add("Houston - Rockets");
+                comboBox3.Items.Add("Memphis - Grizzlies");
+                comboBox3.Items.Add("New Orleans - Hornets");
+                comboBox3.Items.Add("San Antonio - Spurs");
+            }
+            else if(comboBox2.SelectedItem.ToString() == "Atlántico")
+            {
+                comboBox3.Items.Add("Boston - Celtics");
+                comboBox3.Items.Add("Brooklyn - Nets");
+                comboBox3.Items.Add("New York - Knicks");
+                comboBox3.Items.Add("Philadelphia - 76ers");
+                comboBox3.Items.Add("Toronto Raptors");
+            }
+            else if(comboBox2.SelectedItem.ToString() == "Central")
+            {
+                comboBox3.Items.Add("Chicago - Bulls");
+                comboBox3.Items.Add("Cleveland - Cavaliers");
+                comboBox3.Items.Add("Detroit - Pistons");
+                comboBox3.Items.Add("Indiana - Pacers");
+                comboBox3.Items.Add("Milwaukee - Bucks");
+            }
+            else if(comboBox2.SelectedItem.ToString() == "Sureste")
+            {
+                comboBox3.Items.Add("Atlanta - Hawks");
+                comboBox3.Items.Add("Charlotte - Hornets");
+                comboBox3.Items.Add("Miami - Heat");
+                comboBox3.Items.Add("Orlando - Magics");
+                comboBox3.Items.Add("Washington - Wizards");
+            }
+        }
+
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Visible = true;
+
+            if (comboBox3.SelectedItem.ToString() == "Denver - Nuggets")
+                pictureBox1.Image = combox_Demo.Properties.Resources.Nuggets;
+            else if (comboBox3.SelectedItem.ToString() == "San Francisco - Warriors")
+                pictureBox1.Image = combox_Demo.Properties.Resources.Wariors;
+            else if (comboBox3.SelectedItem.ToString() == "Dallas - Mavericks")
+                pictureBox1.Image = combox_Demo.Properties.Resources.Mavericks;
+        }
+    }
+}
