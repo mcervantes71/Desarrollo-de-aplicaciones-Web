4c1349922d13c2719fd8ad0c1fa2bd1bfc13b746
Martin Cervantes <cervantes.martine@gmail.com>
2018-07-09 20:30:28 -0600

    wpf comboBox listBox demo

---
+++ 111 C#/Wpf ComboBox Demo/Wpf ComboBox Demo/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf_ComboBox_Demo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Dates_OnSelectChange(object sender, SelectionChangedEventArgs e)
+        {
+            IList items = e.AddedItems;
+            int index = Dates.SelectedIndex;
+            object selectedItem = Dates.SelectedItem;
+            object selectedValue = Dates.SelectedValue;
+
+            Debug.WriteLine("\n* * * * * * * * * * * * * * * * * * * * * * * * *\n");
+            Debug.WriteLine("AddedItem:{0}", (DateTime)items[0]);
+            Debug.WriteLine("SeledtedIndex:{0}", index);
+            Debug.WriteLine("SeledtedItem:{0}", (DateTime)selectedItem);
+            //Debug.WriteLine("SeledtedValue:{0}", (DayOfWeek)selectedValue);
+
+        }
+    }
+}
