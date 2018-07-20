3101e4110784f436c5161430a1ee95faac4c5e6d
Martin Cervantes <cervantes.martine@gmail.com>
2018-07-20 07:54:44 -0600

    jquery ToDoList

---
+++ jQuery/ToDoList/js/script.js
@@ -0,0 +1,24 @@
+$("ul").on("click", "li", function() {
+    // if($(this).css("color") === "rgb(0, 0, 0)")
+    //     $(this).css({color: "gray", textDecoration: "line-through"});
+    // else
+    //     $(this).css({color: "black", textDecoration: "none"});
+
+    $(this).toggleClass("completed");
+});
+
+$("ul").on("click", "span", function(event) {
+    $(this).parent().fadeOut(500, function() {
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+
+$("input[type='text']").keypress(function(event) {
+    if(event.which === 13){
+        var activity = $(this).val();
+        $(this).val("");
+        $("ul").append("<li><span>X</span>" + activity + "</li>");
+    }
+});
