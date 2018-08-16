7796ee2b9e6c79fafad3a860a02bd611975e527d
Martin Cervantes <cervantes.martine@gmail.com>
2018-08-16 12:03:13 -0600

    proyecto clinica

---
+++ 222 PHP/Clinica/js/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('select');
+    var instances = M.FormSelect.init(elems, options);
+});
+
+$(document).ready(function(){
+    $('select').formSelect();
+    $('.datepicker').datepicker();
+});
