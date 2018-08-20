2daf0e743506c6bace93b324b06f54a032db1be1
Martin Cervantes <cervantes.martine@gmail.com>
2018-08-20 13:09:04 -0600

    clinica

--- 222 PHP/Clinica/js/script.js
+++ 222 PHP/Clinica/js/script.js
@@ -1,6 +1,11 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.datepicker');
-    var instances = M.Datepicker.init(elems, options);
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, options);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.collapsible');
+    var instances = M.Collapsible.init(elems, options);
 });
 
 document.addEventListener('DOMContentLoaded', function() {
@@ -8,7 +13,14 @@ document.addEventListener('DOMContentLoaded', function() {
     var instances = M.FormSelect.init(elems, options);
 });
 
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems, options);
+});
+
 $(document).ready(function(){
+    $('.sidenav').sidenav();
+    $('.collapsible').collapsible();
     $('select').formSelect();
     $('.datepicker').datepicker();
 });
