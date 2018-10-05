9ab985f8ad9dc11f6d372b70d3b44a7f3325b483
Martin Cervantes <cervantes.martine@gmail.com>
2018-10-05 13:56:56 -0600

    wpf layout process

---
+++ 111 C#/Wpf Layout Process/Wpf Layout Process/HorizontalStackPanel.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf_Layout_Process
+{
+    class HorizontalStackPanel:Panel
+    {
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            var size = new Size();
+
+            foreach(UIElement child in this.InternalChildren)
+            {
+                child.Measure(availableSize);
+                size.Width += child.DesiredSize.Width;
+                size.Height = size.Height > child.DesiredSize.Height ? size.Height : child.DesiredSize.Height;
+            }
+
+            return size;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            var location = new Point();
+
+            foreach(UIElement child in this.InternalChildren)
+            {
+                child.Arrange(new Rect(location, child.DesiredSize));
+                location.X += child.DesiredSize.Width;
+            }
+            return finalSize;
+        }
+    }
+}
---
+++ 111 C#/Wpf Layout Process/Wpf Layout Process/Positioning_UI_Elements.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Wpf_Layout_Process
+{
+    /// <summary>
+    /// Interaction logic for Positioning_UI_Elements.xaml
+    /// </summary>
+    public partial class Positioning_UI_Elements : Window
+    {
+        public Positioning_UI_Elements()
+        {
+            InitializeComponent();
+        }
+    }
+}
