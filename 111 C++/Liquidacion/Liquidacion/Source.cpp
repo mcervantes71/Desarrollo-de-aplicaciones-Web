10dbe0a27f933c1da63cdd32da35d387a9150e54
Martin Cervantes <cervantes.martine@gmail.com>
2018-11-04 12:08:58 -0700

    c++ liquidacion

--- 111 C++/Liquidacion/Liquidacion/Source.cpp
+++ 111 C++/Liquidacion/Liquidacion/Source.cpp
@@ -17,9 +17,38 @@ void Proporcional_Aguinaldo()
     cout << "Tu proporcional de aguinaldo es " << aguinaldo << endl;
 }
 
+void Prima_Antiguedad()
+{
+    float salario_tope, prima_antiguedad = 0;
+    int factor_antiguedad, años_trabajados, dias_por_año = 12;
+
+    cout << "Cual es el salario tope ??" << endl;
+    cin >> salario_tope;
+
+    cout << "Cuantos son los años trabajados ??" << endl;
+    cin >> años_trabajados;
+
+    if (años_trabajados >= 10)
+        factor_antiguedad = 10;
+    else if (años_trabajados >= 7 && años_trabajados < 10)
+        factor_antiguedad = 7;
+    else if (años_trabajados >= 5 && años_trabajados < 7)
+        factor_antiguedad = 5;
+    else if (años_trabajados >= 3 && años_trabajados < 5)
+        factor_antiguedad = 3;
+    else if (años_trabajados < 3)
+        factor_antiguedad = 1;
+
+    prima_antiguedad = float(salario_tope * factor_antiguedad * dias_por_año);
+
+    cout << "Prima de Antiguedad " << prima_antiguedad << endl;
+}
+
 void main()
 {
-    Proporcional_Aguinaldo();
+    //Proporcional_Aguinaldo();
+
+    Prima_Antiguedad();
 
     system("pause");
 }
