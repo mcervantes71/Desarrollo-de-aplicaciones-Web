dd83f1ba4b28cc593cc2858110f10344bb6fc562
Martin Cervantes <cervantes.martine@gmail.com>
2019-07-24 19:14:55 -0600

    weather

---
+++ 222 NodeJS/Chalk_app/app.js
@@ -0,0 +1,8 @@
+const chalk = require('chalk')
+
+
+console.log(chalk.green('Success!! :D'))
+
+console.log(chalk.green.bgBlue.bold('Hello World!! ;)'))
+
+console.log(chalk.red.inverse.bold('Error'))
---
+++ 222 NodeJS/Weather_app/app.js
@@ -0,0 +1,11 @@
+console.log('Starting')
+
+setTimeout(() =>{
+    console.log('Waiting 2 seconds')
+}, 2000)
+
+setTimeout(() =>{
+    console.log('0 seconds')
+}, 0)
+
+console.log('Stopping')
