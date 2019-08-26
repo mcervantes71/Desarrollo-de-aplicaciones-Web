b1e4e0514ef40c73394b2aa885f39c68d5a3571e
Martin Cervantes <cervantes.martine@gmail.com>
2019-08-26 17:09:52 -0600

    n queens console

---
+++ 111 C#/N Queens Console/Position.cs
@@ -0,0 +1,69 @@
+﻿#define guess
+
+using System;
+using System.Collections;
+
+namespace EightQueensConsole
+{
+    public class Position
+    {
+#if guess
+        public static int NumberOfQueens;
+        public static ArrayList resultados = new ArrayList();
+#else
+        public static int NumberOfQueens = 8;
+#endif
+        public static int NumberOfAttempts, NumberOfNodes, NumberOfSolutions;
+        private static Position queenAbove;
+        private int line, row;
+        private Position parent;
+
+        public Position(int Line, int Row, Position Parent)
+        {
+            line = Line;
+            row = Row;
+            parent = Parent;
+            NumberOfNodes++;
+        }
+
+        public void WalkThroughTree()
+        {
+            if (line == NumberOfQueens) 
+            {
+                PrintSolution(this);
+                return;
+            }
+
+            for (var r = 0; r < NumberOfQueens; r++) 
+            {
+                NumberOfAttempts++;
+                queenAbove = this;
+                while (queenAbove.row >= 0 && r != queenAbove.row // vertical
+                        && r - queenAbove.row != line + 1 - queenAbove.line // diagonal left
+                        && queenAbove.row - r != line + 1 - queenAbove.line) // diagonal right
+                    queenAbove = queenAbove.parent; // repeat check for all queens in previous lines
+                        if (queenAbove.line == 0) // no threat found
+                            new Position(line + 1, r, this).WalkThroughTree(); // put queen on next line
+            }
+        }
+
+        private static void PrintSolution(Position Node)
+        {
+            NumberOfSolutions++;
+
+            string cadena = "";
+
+            while (Node.row >= 0)
+            {
+                cadena += (char)('a' + Node.row);
+
+                Node = Node.parent;
+            }
+
+            Console.WriteLine(cadena);
+#if guess
+            resultados.Add(cadena);
+#endif
+        }
+    }
+}
---
+++ 111 C#/N Queens Console/Program.cs
@@ -0,0 +1,63 @@
+﻿#define guess
+
+using System;
+
+namespace EightQueensConsole
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+#if guess
+            string resp = "s";
+            string guess = "";
+            int counter = 0;
+
+            do
+            {
+                Position.NumberOfAttempts = 0;
+                Position.NumberOfNodes = 0;
+                Position.NumberOfQueens = 0;
+                Position.NumberOfSolutions = 0;
+
+                Console.Clear();
+
+                Console.WriteLine("Introduce una posible solucion");
+                guess = Console.ReadLine();
+
+                Position.NumberOfQueens = guess.Length;
+#endif
+                long startTime = DateTime.Now.Ticks;
+
+                new Position(0, Int32.MinValue, null).WalkThroughTree();
+#if guess
+                foreach (string res in Position.resultados)
+                {
+                    counter = 0;
+
+                    for (int i = 0; i < guess.Length; i++)
+                        if (res[i] == guess[i])
+                            counter += 1;
+                        else
+                            counter -= 1;
+
+                    Console.WriteLine(counter.ToString());
+                }
+
+                Position.resultados.Clear();
+#endif
+                Console.WriteLine("Milliseconds: " + (DateTime.Now.Ticks - startTime) / TimeSpan.TicksPerMillisecond);
+                Console.WriteLine("Queens/Squares: " + Position.NumberOfQueens.ToString("#,##0"));
+                Console.WriteLine("Attempts: " + Position.NumberOfAttempts.ToString("#,##0"));
+                Console.WriteLine("Nodes: " + (Position.NumberOfNodes - 1).ToString("#,##0"));
+                Console.WriteLine("Solutions: " + Position.NumberOfSolutions.ToString("#,##0"));
+#if guess
+                Console.WriteLine("\n\nDesea volver ?? (s/n)");
+                resp = Console.ReadLine();
+            } while (resp == "s");
+#endif
+            Console.WriteLine("\n\n* * *   Fin del Programa   * * *");
+            Console.ReadLine();
+        }
+    }
+}
