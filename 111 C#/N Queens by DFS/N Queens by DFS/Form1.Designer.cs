0a36d18cd0eb275cf698a220ae065de42be30bed
Martin Cervantes <cervantes.martine@gmail.com>
2019-08-31 17:14:04 -0600

    n queens by DFS

---
+++ 111 C#/N Queens by DFS/N Queens by DFS/Form1.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace N_Queens_by_DFS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.txtN = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.btnStart = new System.Windows.Forms.Button();
+            this.pnlChess = new System.Windows.Forms.Panel();
+            this.SuspendLayout();
+            // 
+            // txtN
+            // 
+            this.txtN.Location = new System.Drawing.Point(198, 12);
+            this.txtN.Name = "txtN";
+            this.txtN.Size = new System.Drawing.Size(100, 20);
+            this.txtN.TabIndex = 0;
+            this.txtN.Text = "4";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(93, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero de Reinas";
+            // 
+            // lblTime
+            // 
+            this.lblTime.AutoSize = true;
+            this.lblTime.Location = new System.Drawing.Point(69, 43);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(77, 13);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "0 Milisegundos";
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(210, 38);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Iniciar";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // pnlChess
+            // 
+            this.pnlChess.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pnlChess.Location = new System.Drawing.Point(30, 70);
+            this.pnlChess.Name = "pnlChess";
+            this.pnlChess.Size = new System.Drawing.Size(550, 550);
+            this.pnlChess.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(609, 651);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtN);
+            this.Controls.Add(this.pnlChess);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "N Queens by DFS";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtN;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Panel pnlChess;
+    }
+}
+
---
+++ 111 C#/N Queens by DFS/N Queens by DFS/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace N_Queens_by_DFS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                pnlChess.Controls.Clear();
+
+                if (txtN.Text != "")
+                {
+                    int N = int.Parse(txtN.Text);
+
+                    Board board = new Board(N);
+
+                    System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+
+                    lblTime.Text = "";
+
+                    sw.Start();
+
+                    Board res = DFS(board);
+
+                    sw.Stop();
+
+                    lblTime.Text = sw.ElapsedMilliseconds.ToString() + "  MilliSecond";
+
+                    if (res != null)
+                    {
+                        res.ShowBoard(pnlChess);
+                    }
+                    else
+                    {
+                        MessageBox.Show("No Result");
+                    }
+                }
+            }
+            catch
+            {
+
+            }
+        }
+
+        Board DFS(Board board)
+        {
+            if (board.IsGoal() == true)
+            {
+                return board;
+            }
+            else
+            {
+                for (int i = 0; i < board.N; i++)
+                {
+                    if (board.isSafe(i))
+                    {
+                        board.Place(i);
+
+                        Board res = DFS(board);
+
+                        if (res != null)
+                            return res;
+
+                        board.UnPlace();
+                    }
+                }
+            }
+
+            return null;
+        }
+    }
+
+    class Board
+    {
+        public int N;
+        int[] board;
+        public int cnt;
+
+        public Board(int n)
+        {
+            N = n;
+
+            board = new int[N];
+
+            for (int i = 0; i < N; i++)
+                board[i] = 0;
+            cnt = 0;
+        }
+
+        public bool isSafe(int Clm)
+        {
+            for (int i = 0; i < cnt; i++)
+                if ((board[i] == Clm) || Math.Abs(Clm - board[i]) == (cnt - i))
+                    return false;
+
+            return true;
+        }
+
+        public void Place(int Clm)
+        {
+            if (Clm >= 0 && Clm < N)
+            {
+                board[cnt] = Clm;
+                cnt++;
+            }
+            else
+            {
+                MessageBox.Show("Bad Column");
+            }
+        }
+
+        public bool IsGoal()
+        {
+            if (cnt == N)
+                return true;
+            else
+                return false;
+        }
+
+        public void UnPlace()
+        {
+            if (cnt > 0)
+                cnt--;
+        }
+
+        public void ShowBoard(Panel pnl)
+        {
+            pnl.Controls.Clear();
+
+            PictureBox[,] chess = new PictureBox[N, N];
+
+            int w = 0, h = 0;
+
+            w = pnl.Width;
+            h = pnl.Height;
+
+            int xs = (int)((double)w / (double)N);
+            int ys = (int)((double)h / (double)N);
+
+            for (int i = 0; i < N; i++)
+                for (int j = 0; j < N; j++)
+                {
+                    chess[i, j] = new PictureBox();
+                    chess[i, j].BorderStyle = BorderStyle.FixedSingle;
+                    chess[i, j].Parent = pnl;
+                    chess[i, j].Location = new Point(j * xs + 1, i * ys + 1);
+                    chess[i, j].Size = new Size(xs, ys);
+
+                    if ((i + j) % 2 == 0)
+                        chess[i, j].BackColor = Color.White;
+                    else
+                        chess[i, j].BackColor = Color.Black;
+                }
+
+            for (int i = 0; i < cnt; i++)
+            {
+                chess[i, board[i]].Image = N_Queens_by_DFS.Properties.Resources.queen;
+                chess[i, board[i]].SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+        }
+
+    }
+}
