4492e7e07a12a844835aadc938a2ab489ee37257
Martin Cervantes <cervantes.martine@gmail.com>
2019-10-16 21:14:41 -0600

    clinica

--- 222 PHP/Clinica/js/script.js
+++ 222 PHP/Clinica/js/script.js
@@ -24,3 +24,115 @@ $(document).ready(function(){
     $('select').formSelect();
     $('.datepicker').datepicker();
 });
+
+
+/**********************************************************************/
+/**********************************************************************/
+/**********************************************************************/
+let total_historial;
+let nombre = 0, edad = 0, sexo = 0, direccion = 0, telefono = 0;
+
+function Check_Historial_Clinico()
+{
+    total_historial = nombre + edad + sexo + direccion + telefono;
+
+    if(total_historial === 0)
+    {
+        console.log("Empty Historial Clinico");
+        $("#historial_clinico").removeClass("white-text green");
+        $("#historial_clinico").removeClass("yellow");
+        $("#historial_clinico").addClass("grey lighten-1");
+        $("#historial_clinico > i").text("edit");
+    }
+    else if(total_historial < 5)
+    {
+        console.log("Missing something in Historial Clinico");
+        $("#historial_clinico").removeClass("white-text green");
+        $("#historial_clinico").removeClass("grey lighten-1");
+        $("#historial_clinico").addClass("yellow");
+        $("#historial_clinico > i").text("error");
+    }
+    else if(total_historial === 5)
+    {
+        console.log("Historial Clinico Done");
+        $("#historial_clinico").removeClass("yellow");
+        $("#historial_clinico").removeClass("grey lighten-1");
+        $("#historial_clinico").addClass("white-text green");
+        $("#historial_clinico > i").text("done");
+    }
+}
+
+$("#nombre").on("input", function(){
+    if($("#nombre").val() != "")
+    {
+        console.log("Nombre: " + $("#nombre").val());
+        nombre = 1;
+    }
+    else
+    {
+        console.log("The name is empty :(");
+        nombre = 0;
+    }
+
+    Check_Historial_Clinico();
+});
+
+$("#edad").on("input", function(){
+    if($("#edad").val() != "")
+    {
+        console.log("Edad: " + $("#edad").val());
+        edad = 1;
+    }
+    else
+    {
+        console.log("The edad is empty :(");
+        edad = 0;
+    }
+
+    Check_Historial_Clinico();
+});
+
+$("#sexo").change(function(){
+    if($("#sexo").val() != "")
+    {
+        console.log("sexo: " + $("#sexo").children("option:selected").val());
+        sexo = 1;
+    }
+    else
+    {
+        console.log("The sexo is empty :(");
+        sexo = 0;
+    }
+
+    Check_Historial_Clinico();
+});
+
+$("#direccion").on("input", function(){
+    if($("#direccion").val() != "")
+    {
+        console.log("direccion: " + $("#direccion").val());
+        direccion = 1;
+    }
+    else
+    {
+        console.log("The direccion is empty :(");
+        direccion = 0;
+    }
+
+    Check_Historial_Clinico();
+});
+
+$("#telefono").on("input", function(){
+    if($("#telefono").val() != "")
+    {
+        console.log("telefono: " + $("#telefono").val());
+        telefono = 1;
+    }
+    else
+    {
+        console.log("The telefono is empty :(");
+        telefono = 0;
+    }
+
+    Check_Historial_Clinico();
+});
