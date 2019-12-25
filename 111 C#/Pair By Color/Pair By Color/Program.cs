9aa7b857d20c742fe05c43d339cbda5e93cb33bf
Martin Cervantes <cervantes.martine@gmail.com>
2019-12-24 19:23:49 -0700

    Add files via upload

---
+++ 111 C#/Pair By Color/Pair By Color/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+
+namespace Pair_By_Color
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 9;
+            int[] ar = { 10, 20, 20, 10, 10, 30, 50, 10, 20 };
+
+            int odds = 0;
+            int index = 0;
+            ArrayList numbers = new ArrayList();
+            ArrayList counters = new ArrayList();
+
+
+            for (int i = 0; i < n; i++)
+            {
+                if (numbers.IndexOf(ar[i]) == -1)
+                {
+                    numbers.Add(ar[i]);
+                    counters.Add(1);
+                }
+                else
+                {
+                    index = numbers.IndexOf(ar[i]);
+                    counters[index] = Convert.ToInt32(counters[index]) + 1;
+                }
+            }
+
+            foreach (int count in counters)
+            {
+                odds += Convert.ToInt32(count) % 2;
+            }
+
+            Console.WriteLine(odds);
+        }
+    }
+}
