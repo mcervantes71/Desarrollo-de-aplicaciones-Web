4714acc56159cfdbf084701044ce59eccaab2164
Martin Cervantes <cervantes.martine@gmail.com>
2020-07-08 12:50:40 -0600

    Fix javascript projects format

--- JavaScript/Factorial/js/script.js
+++ JavaScript/Factorial/js/script.js
@@ -1,12 +1,14 @@
 function factorial(n)
 {
-    if (n % 1 == 0){
+    if (n % 1 == 0)
+    {
         if (n > 0)
             return n * factorial(n - 1);
         else
             return 1;
     }
-    else {
+    else
+    {
         return -1;
     }
 }
