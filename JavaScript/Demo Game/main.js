e52bdccf01e80dd7a5d7b12f528908142792f634
Martin Cervantes <cervantes.martine@gmail.com>
2020-09-03 15:50:06 -0600

    Add background to scene

--- JavaScript/Demo Game/main.js
+++ JavaScript/Demo Game/main.js
@@ -1,5 +1,13 @@
 let gameScene = new Phaser.Scene('Game');
 
+gameScene.preload = function() {
+  this.load.image('background', 'img/background.png');
+}
+
+gameScene.create = function() {
+  this.add.sprite(320, 180, 'background');
+}
+
 let config = {
   type: Phaser.AUTO,
   width: 640,
